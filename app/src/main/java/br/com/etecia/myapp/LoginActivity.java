bac4d8657bf5021fa1fecb15d50072cb8c97c26a
lndsilva <lndsilva@hotmail.com>
2023-08-24 22:04:08 -0300

    layout dentro de outro layout

---
+++ app/src/main/java/br/com/etecia/myapp/LoginActivity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_layout);
+    }
+}
